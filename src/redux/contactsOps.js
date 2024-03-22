7af769b1193928a609d8662d48147e9d16d79396
Irinaaissa <irinaalisa7506@gmail.com>
2024-03-22 21:01:38 +0200

    delet

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -14,7 +14,7 @@ async (_, thunkAPI) => {
 export const deleteContact = createAsyncThunk("contacts/deleteContact",
 async (contactId,thunkAPI) => {
 try {
-    const response = await axios.get(`/contacts/${contactId}`); 
+    const response = await axios.delete(`/contacts/${contactId}`); 
 return response.data;
 } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
